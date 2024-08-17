35dc5a9e2bf0eb064f0b9e1016361befc001f876
masysz <67843831+masysz@users.noreply.github.com>
2024-08-17 10:09:31 +0700

    Update contracts.ts

--- const/contracts.ts
+++ const/contracts.ts
@@ -5,11 +5,11 @@ import client from "@/lib/client";
 // First, import the chain from the package, then set the NETWORK variable to the chain.
 import { getContract } from "thirdweb";
 import { sepolia } from "thirdweb/chains";
-export const NETWORK = sepolia;
+export const NETWORK = base;
 
 // 2. The address of the marketplace V3 smart contract.
 // Deploy your own: https://thirdweb.com/thirdweb.eth/MarketplaceV3
-const MARKETPLACE_ADDRESS = "0x38ab4489E479c9266471bbe8C3794CB30EA11F20";
+const MARKETPLACE_ADDRESS = "0xCc88d7CB074012B1DcB6fe6FD93498cdCEBfE5f3";
 export const MARKETPLACE = getContract({
 	address: MARKETPLACE_ADDRESS,
 	client,
@@ -17,7 +17,7 @@ export const MARKETPLACE = getContract({
 });
 
 // 3. The address of your NFT collection smart contract.
-const NFT_COLLECTION_ADDRESS = "0x72a6eb347D86Bb5DE9c3c6a3DFAb6f2eff80F3C9";
+const NFT_COLLECTION_ADDRESS = "0xAB3c186aB572953a76ED8daEE611C2730B86be2c";
 export const NFT_COLLECTION = getContract({
 	address: NFT_COLLECTION_ADDRESS,
 	client,
@@ -26,4 +26,4 @@ export const NFT_COLLECTION = getContract({
 
 // (Optional) Set up the URL of where users can view transactions on
 // For example, below, we use Mumbai.polygonscan to view transactions on the Mumbai testnet.
-export const ETHERSCAN_URL = "https://sepolia.etherscan.io";
+export const ETHERSCAN_URL = "https://basescan.org";
