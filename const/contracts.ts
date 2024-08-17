f6c8942122114e3b824cef560360d5a132190ee4
masysz <67843831+masysz@users.noreply.github.com>
2024-08-17 10:09:57 +0700

    Update contracts.ts

--- const/contracts.ts
+++ const/contracts.ts
@@ -4,7 +4,7 @@ import client from "@/lib/client";
 // 1. Set up the network your smart contracts are deployed to.
 // First, import the chain from the package, then set the NETWORK variable to the chain.
 import { getContract } from "thirdweb";
-import { sepolia } from "thirdweb/chains";
+import { base } from "thirdweb/chains";
 export const NETWORK = base;
 
 // 2. The address of the marketplace V3 smart contract.
